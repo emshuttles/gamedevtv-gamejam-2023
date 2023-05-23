bf5c9c17dc1e51d5ac2ddbb8b422f21b9771412a
AD1337 <lucasgmolina@gmail.com>
2023-05-23 14:33:28 -0300

    persometer dragging and open/close

--- src/components/draggable.gd
+++ src/components/draggable.gd
@@ -1,20 +1,25 @@
-extends Area2D
+class_name Draggable
+extends Node2D
 
 
 var _is_held := false
+var click_position:Vector2
 
 onready var parent:Node = get_parent()
 
+signal drag_ended()
+
 
 func _ready() -> void:
-	connect("input_event",self,"_on_input_event")
+	parent.connect("input_event",self,"_on_input_event")
+	
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		event = event as InputEventMouseMotion
 		if _is_held:
-			parent.global_position = event.global_position
+			owner.global_position = event.position - click_position
 			get_tree().set_input_as_handled()
 
 
@@ -26,3 +31,8 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 				if event.pressed
 				else Input.MOUSE_MODE_VISIBLE)
 		Input.mouse_mode = mouse_mode
+		click_position = owner.get_local_mouse_position()
+		
+		if not event.pressed:
+			_is_held = false
+			emit_signal("drag_ended")

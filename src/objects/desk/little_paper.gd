030c3779c2db092b327217a5c8f4a8aa1a14aba0
Erik Shuttlesworth <erik.shuttlesworth@gmail.com>
2023-05-23 15:26:11 +0100

    Prevent dragging paper outside window

--- src/objects/desk/little_paper.gd
+++ src/objects/desk/little_paper.gd
@@ -12,7 +12,12 @@ func _unhandled_input(event: InputEvent) -> void:
 			get_tree().set_input_as_handled()
 
 
+# TODO: Need to keep paper on desk, not just in window
 func _on_LittlePaper_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		event = event as InputEventMouseButton
 		_is_held = event.pressed
+		var mouse_mode: int = (Input.MOUSE_MODE_CONFINED
+				if event.pressed
+				else Input.MOUSE_MODE_VISIBLE)
+		Input.mouse_mode = mouse_mode

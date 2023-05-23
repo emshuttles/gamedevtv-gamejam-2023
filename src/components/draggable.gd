4d1d4979883dfab2be37af800f65819e20074e49
Erik Shuttlesworth <erik.shuttlesworth@gmail.com>
2023-05-23 23:50:45 +0100

    Add zoom levels; make Draggable component

--- src/components/draggable.gd
+++ src/components/draggable.gd
@@ -1,31 +1,43 @@
-extends Area2D
+class_name Draggable
+extends Node
 
 
 var _is_mouse_inside := false
 var _is_held := false
 var _click_position: Vector2
 
+onready var _parent: Node = get_parent()
 
+
+func _ready() -> void:
+	_parent.connect("mouse_entered", self, "_on_mouse_entered")
+	_parent.connect("mouse_exited", self, "_on_mouse_exited")
+
+
+# TODO: How to put most recently moved paper on top?
 func _unhandled_input(event: InputEvent) -> void:
 	if _is_mouse_inside and event.is_action("select"):
 		event = event as InputEventMouseButton
 		_is_held = event.pressed
+
+		# To keep mouse inside window when dragging
 		var mouse_mode: int = (Input.MOUSE_MODE_CONFINED
 				if event.pressed
 				else Input.MOUSE_MODE_VISIBLE)
 		Input.mouse_mode = mouse_mode
-		_click_position = get_local_mouse_position()
+
+		_click_position = owner.get_local_mouse_position()
 		get_tree().set_input_as_handled()
 	elif event is InputEventMouseMotion:
 		event = event as InputEventMouseMotion
 		if _is_held:
-			global_position = event.position - _click_position
+			owner.global_position = owner.get_global_mouse_position() - _click_position
 			get_tree().set_input_as_handled()
 
 
-func _on_LittlePaper_mouse_entered() -> void:
+func _on_mouse_entered() -> void:
 	_is_mouse_inside = true
 
 
-func _on_LittlePaper_mouse_exited() -> void:
+func _on_mouse_exited() -> void:
 	_is_mouse_inside = false
--- src/objects/zooming_camera.gd
+++ src/objects/zooming_camera.gd
@@ -3,23 +3,28 @@ extends Camera2D
 
 const CENTER_POSITION := Vector2(512, 300)
 
-export var zoom_level := 0.25
+export var zoom_levels := [0.25, 0.5, 0.75, 1.0]
 export var zoom_duration := 0.2
 
-var _is_zoomed := false
+var _zoom_level_index := 3
 
 onready var tween: Tween = $Tween
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("zoom"):
+	if event.is_action_pressed("zoom_in"):
 		event = event as InputEventMouseButton
-		_zoom(event.global_position)
+		_zoom_level_index = max(_zoom_level_index - 1, 0)
+		var new_zoom_level: float = zoom_levels[_zoom_level_index]
+		_zoom(new_zoom_level, event.global_position)
+	elif event.is_action_pressed("zoom_out"):
+		event = event as InputEventMouseButton
+		_zoom_level_index = min(_zoom_level_index + 1, 3)
+		var new_zoom_level: float = zoom_levels[_zoom_level_index]
+		_zoom(new_zoom_level, event.global_position)
 
 
-func _zoom(click_position: Vector2) -> void:
-	_is_zoomed = not _is_zoomed
-	var new_zoom_level: float = zoom_level if _is_zoomed else 1.0
+func _zoom(new_zoom_level: float, click_position: Vector2) -> void:
 	tween.interpolate_property(
 		self,
 		"zoom",
@@ -30,7 +35,8 @@ func _zoom(click_position: Vector2) -> void:
 		tween.EASE_OUT
 	)
 	var zoom_position: Vector2 = _calculate_zoom_position(click_position)
-	var new_position: Vector2 = zoom_position if _is_zoomed else CENTER_POSITION
+	var is_zoomed: bool = new_zoom_level < zoom_levels[3]
+	var new_position: Vector2 = zoom_position if is_zoomed else CENTER_POSITION
 	tween.interpolate_property(
 		self,
 		"global_position",

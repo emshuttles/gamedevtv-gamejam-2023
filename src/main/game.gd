b7a95e2c393560f6614043bd0213ccab76fe3bd5
AD1337 <lucasgmolina@gmail.com>
2023-05-24 10:38:13 -0300

    transitions etc

---
+++ src/main/game.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+func _ready() -> void:
+	Signals.connect("scene_transition",self,"new_scene")
+
+
+func new_scene(packed_scene:PackedScene):
+	var scene:Node = packed_scene.instance()
+	add_child(scene)

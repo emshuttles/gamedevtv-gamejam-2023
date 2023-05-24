52cacd85815fa20e512c91e96388d49315ac10fe
AD1337 <lucasgmolina@gmail.com>
2023-05-24 11:07:06 -0300

    skip fix

--- src/objects/day_x/day_x.gd
+++ src/objects/day_x/day_x.gd
@@ -1,12 +1,13 @@
 extends MarginContainer
 
 
+var has_skipped:bool = false
+
 onready var animation_player:AnimationPlayer = $"%AnimationPlayer"
 
 
 func _unhandled_input(event: InputEvent) -> void:
-#	skip()
-	pass
+	skip()
 
 
 func _input(event: InputEvent):
@@ -16,8 +17,10 @@ func _input(event: InputEvent):
 
 
 func skip():
-	var animation_last_second:float = animation_player.current_animation_length - 1
-	animation_player.seek(animation_last_second)
+	if not has_skipped:
+		has_skipped = true
+		var animation_last_second:float = animation_player.current_animation_length - 1
+		animation_player.seek(animation_last_second)
 
 
 func change_scene():
